a985ee55aa30472a82a5b332df25f7e759409a3f
kirill-iosdev <kirill.iosdev@gmail.com>
2022-05-16 19:43:53 +0300

    Create save emoji VC

---
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewEmojiTableViewController.swift
+//  EmojiReader
+//
+//  Created by Kirill on 16.05.2022.
+//
+
+import UIKit
+
+class NewEmojiTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
