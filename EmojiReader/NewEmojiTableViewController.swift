89278da38dc1c6730263aa662f0dbe15ef3aa622
kirill-iosdev <kirill.iosdev@gmail.com>
2022-05-20 21:19:21 +0300

    create new viewController

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
 
+    @IBOutlet weak var emojiTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+    }
+    
     
 }
